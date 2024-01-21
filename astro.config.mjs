786a060de8aa7dacbf6867612b9892209e377ab0
Ibrahim Odev <ibrahimodev42@gmail.com>
2024-01-21 14:59:52 +0300

    Init Project :tada:

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://ibrahimo.dev",
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
+  prefetch: {
+    prefetchAll: true
+  }
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+import getPalette from "tailwindcss-palette-generator";
+
+const palette = getPalette([
+	{
+		name: "primary",
+		color: "#00ADB5"
+	},
+	{
+		name: "secondary",
+		color: "#393E46"
+	},
+	{
+		name: "dark",
+		color: "#222831"
+	},
+	{
+		name: "light",
+		color: "#EEEEEE"
+	}
+])
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: palette
+		},
+	},
+	plugins: [],
+}
