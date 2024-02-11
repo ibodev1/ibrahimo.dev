bbf996846fea14819807cc65863d2852392a92e5
Ibrahim Odev <ibrahimodev42@gmail.com>
2024-02-11 16:17:50 +0300

    :star:  robots.txt update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ibrahimo.dev",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), robotsTxt(), sitemap(), tailwind()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
