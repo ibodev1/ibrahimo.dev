fe7468a00296b6fcac4ebfb98464b55c68690d0a
Ibrahim Odev <ibrahimodev42@gmail.com>
2024-02-11 16:13:36 +0300

    :100: social media links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
