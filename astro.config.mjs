2d67fd147ae6786c1271b9f534241cec1dd8986c
Ibrahim Odev <ibrahimodev42@gmail.com>
2024-04-02 22:41:25 +0300

    feat(prettier) formatter prettier added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import robotsTxt from 'astro-robots-txt';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ibrahimo.dev",
+  site: 'https://ibrahimo.dev',
   integrations: [mdx(), robotsTxt(), sitemap(), tailwind()],
-  output: "server",
+  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true
