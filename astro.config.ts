eb33c1679baea6dd873693106a3afe44b3bbe3a6
Ibrahim Odev <ibrahimodev42@gmail.com>
2024-06-13 14:42:46 +0300

    astro db web-vitals added

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,15 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import db from "@astrojs/db";
+import webVitals from "@astrojs/web-vitals";
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ibrahimo.dev',
-  integrations: [robotsTxt(), sitemap(), tailwind(), db()],
+  integrations: [robotsTxt(), sitemap(), tailwind(), db(), webVitals()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
