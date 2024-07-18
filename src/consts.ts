86cef85d4d35aeda32b27c949614d38e9ca7388f
Ibrahim Odev <ibrahimodev42@gmail.com>
2024-07-18 10:37:37 +0300

    chore: update versions and fix contact form

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 import backgroudImge from '../public/background.jpg';
 
 export const SITE_TITLE = '@ibodev';
-export const SITE_DESCRIPTION = 'Younger software developer!';
+export const SITE_DESCRIPTION = 'Software Developer';
 export const SITE_IMAGE_PATH = '/background.jpg';
 export const SITE_IMAGE = backgroudImge;
