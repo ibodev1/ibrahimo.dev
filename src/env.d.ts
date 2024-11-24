3ca646c8f73f34780741660c4da81a11b0b4fb68
İbrahim Ödev <developer.ibrahimodev@gmail.com>
2024-11-24 15:15:14 +0300

    chore: spotify depreacted

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv {
-  readonly SPOTIFY_CLIENT_ID: string;
-  readonly SPOTIFY_CLIENT_SECRET: string;
-  readonly SPOTIFY_CLIENT_REFRESH_TOKEN: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
