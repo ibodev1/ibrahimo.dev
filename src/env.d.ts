7ba1bdae2c901c1b59b7dbb865f972c04308c7b7
İbrahim Ödev <developer.ibrahimodev@gmail.com>
2025-01-17 22:22:46 +0300

    fix: env type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv {
-  readonly ASTRO_STUDIO_APP_TOKEN: string;
-  readonly ADMIN_PASS: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
